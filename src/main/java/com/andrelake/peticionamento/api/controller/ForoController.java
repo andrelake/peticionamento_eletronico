d9d830f8f36edc53109fa8d1a09fa317ce3b15cc
andrelake <andre.lake@hotmail.com>
2020-04-26 15:45:30 -0300

    Foro update

--- src/main/java/com/andrelake/peticionamento/api/controller/ForoController.java
+++ src/main/java/com/andrelake/peticionamento/api/controller/ForoController.java
@@ -2,12 +2,14 @@ package com.andrelake.peticionamento.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +43,16 @@ public class ForoController {
 		
 		foro = service.save(foro);
 		return ResponseEntity.status(HttpStatus.CREATED).body(foro);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Foro> update(@PathVariable Long id, @RequestBody Foro foro) {
+		
+		Foro foroRoot = service.findById(id);
+		
+		BeanUtils.copyProperties(foro, foroRoot, "id");
+		
+		service.save(foroRoot);
+		return ResponseEntity.ok(foroRoot);
 	}
 }
