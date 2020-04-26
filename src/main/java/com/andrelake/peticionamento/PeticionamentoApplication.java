6eaa92ac9493e55a6dc4e5e9a4e5b2a26654aa96
andrelake <andre.lake@hotmail.com>
2020-04-26 14:52:53 -0300

    Project created

---
+++ src/main/java/com/andrelake/peticionamento/PeticionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.andrelake.peticionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeticionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeticionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrelake/peticionamento/PeticionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelake.peticionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeticionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
