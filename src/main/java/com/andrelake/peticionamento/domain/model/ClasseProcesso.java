cbf57ee5c8bbe693b3ba15c09d21ac2dfca039a9
andrelake <andre.lake@hotmail.com>
2020-04-26 22:45:51 -0300

    ClasseProcesso entity

--- src/main/java/com/andrelake/peticionamento/domain/model/ClasseProcesso.java
+++ src/main/java/com/andrelake/peticionamento/domain/model/ClasseProcesso.java
@@ -1,13 +1,10 @@
 package com.andrelake.peticionamento.domain.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,7 +12,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Competencia {
+public class ClasseProcesso {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +20,6 @@ public class Competencia {
 	private Long id;
 	private String nome;
 	
-	@OneToMany(mappedBy = "competencia")
-	private List<ClasseProcesso> classes = new ArrayList<>();
+	@ManyToOne
+	private Competencia competencia;
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
 insert into foro (nome) value ('Foro Regional XII - Nossa Senhora do Ó'), ('Fórum Ourinhos');
 
-insert into competencia (nome) value ('Cível'), ('Família e Sucessões'), ('Conciliação');
+insert into competencia (nome) value ('Cível'), ('Família e Sucessões'), ('Criminal');
 
 insert into foro_competencias (foro_id, competencia_id) values (1, 1), (1, 2), (1, 3), (2, 1);
+
+insert into classe_processo (nome, competencia_id) values ('Usucapião', 1), ('Execução de Título Extrajudicial', 1), ('Petição Criminal', 3), ('Adoção c/c Destituição de Poder Familiar', 2);
