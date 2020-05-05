ad1f20a2d523eb9a303e4f631d48b58197c0d7ef
andrelake <andre.lake@hotmail.com>
2020-05-05 15:45:19 -0300

    EstadoCivil validation

--- src/main/java/com/andrelake/peticionamento/api/controller/parte/EstadoCivilController.java
+++ src/main/java/com/andrelake/peticionamento/api/controller/parte/EstadoCivilController.java
@@ -2,6 +2,8 @@ package com.andrelake.peticionamento.api.controller.parte;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -40,14 +42,14 @@ public class EstadoCivilController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<EstadoCivil> insert(@RequestBody EstadoCivil estadoCivil) {
+	public ResponseEntity<EstadoCivil> insert(@RequestBody @Valid EstadoCivil estadoCivil) {
 		
 		estadoCivil = service.save(estadoCivil);
 		return ResponseEntity.status(HttpStatus.CREATED).body(estadoCivil);
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<EstadoCivil> update(@PathVariable Long id, @RequestBody EstadoCivil estadoCivil) {
+	public ResponseEntity<EstadoCivil> update(@PathVariable Long id, @RequestBody @Valid EstadoCivil estadoCivil) {
 		
 		EstadoCivil estadoCivilRoot = service.findById(id);
 		
